3a816308775b7b7f5ed076aee4c1c33bcae89dfe
sousup <31660730+sousup@users.noreply.github.com>
2022-12-06 17:37:35 -0300

    cart v2

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:pet_shop/pages/home/home_page.dart';
+import 'package:pet_shop/provider/cart_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,13 +16,18 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
-        primarySwatch: Colors.blue,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => CartProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+          primarySwatch: Colors.blue,
+        ),
+        home: HomePage(),
       ),
-      home: HomePage(),
     );
   }
 }
---
+++ lib/provider/cart_provider.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+import '../models/cart_model.dart';
+
+class CartProvider with ChangeNotifier {
+  List<CartModel> _carts = [];
+  List<CartModel> get carts => _carts;
+
+  set carts(List<CartModel> carts) {
+    _carts = carts;
+    notifyListeners();
+  }
+}
